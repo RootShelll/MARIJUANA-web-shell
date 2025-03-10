d2a3dc0fc5fed29caa32adb6360b8f154a48c3ba
R00t-Shell <r00t.shell00@gmail.com>
2025-03-10 12:29:07 +0300

    Update MARIJUANA.php
    Understanding the MARIJUANA Web Shell: Features and Security Implications
    Learn about the MARIJUANA web shell, a PHP-based backdoor with stealth capabilities designed to bypass server security measures. Understand its features, potential risks, and implications for cybersecurity.
    MARIJUANA web shell, PHP backdoor, server security, web shell features, cyber threats

--- MARIJUANA.php
+++ MARIJUANA.php
@@ -4,7 +4,7 @@
     |  Author: R00t Shell                                                             |
     |  Date: 2025-02-18 06:45:35                                                      |
     |  Website: https://r00t-shell.com                                                |
-    |  Virus Total: a5ae0aab352871bc0b038b3aa43b03eb223425628c884af7b9fc592cd34eb86c  |
+    |  Virus Total: 1ff50cf31c1e4caa75ec8c1339ef4ab1a0c566a0a8ac4bf871135452765c83f6  |
     |  Description: Obfuscates PHP code to increase security and protect source code. |
     |_________________________________________________________________________________|
 */
